6e493f746e1ce4f4869bea9c0a6cc54ce2ca05b1
LLine <LLine@cad27aaa-dce3-4a30-a00a-e4fd67c11881>
2009-11-15 12:52:37 +0000

    * Added rewritten Arch Bishop and Warlock skills to the trunk/rewrite folder. - Things missing: Editted item_db.txt with support for third class bitmasks. Current job system of stable/r13890, which is cleaner and more functional. - BEWARE! Everything in the trunk folder should be used for testing purposes only, though we do need your help reporting any bugs you might find so we can merge it to their respective /stable folder. * Moved '3CeAM Docunments' folder to svn root. * Added a general Changelog to svn root, which'll contain changes made to all branches.
    git-svn-id: svn://svn.code.sf.net/p/v1-3ceam/code/trunk@13 cad27aaa-dce3-4a30-a00a-e4fd67c11881

---
+++ rewrite/sql-files/upgrade_eAitemdb_to_3CeAMitemdb.sql
@@ -0,0 +1,2 @@
+UPDATE `item_db` SET `equip_upper` = `equip_upper`+24 WHERE `equip_upper` = 7 OR `equip_upper` = 2;
+UPDATE `item_db2` SET `equip_upper` = `equip_upper`+24 WHERE `equip_upper` = 7 OR `equip_upper` = 2;
