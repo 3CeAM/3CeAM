c64eb2d1e547117066bf3815f23f1f8d29ac2724
Jobbie <Jobbie@cad27aaa-dce3-4a30-a00a-e4fd67c11881>
2010-03-13 18:45:04 +0000

    * Fixed an issue where Mechanics cannot equip mado when using NPC
    git-svn-id: https://svn.code.sf.net/p/v1-3ceam/code/trunk@245 cad27aaa-dce3-4a30-a00a-e4fd67c11881

--- rewrite/src/map/pc.c
+++ rewrite/src/map/pc.c
@@ -6926,7 +6926,7 @@ int pc_setriding(TBL_PC* sd, int flag)
 	}
 	if( flag )
 	{		
-		if( option != OPTION_MADO && (pc_checkskill(sd,skillnum) > 0) ) // Check if you have the necessary skill to mount.
+		if( option&OPTION_MADO || (pc_checkskill(sd,skillnum) > 0) ) // Check if you have the necessary skill to mount.
 			pc_setoption(sd, sd->sc.option|option);
 	}
 	else if( pc_isriding(sd, OPTION_RIDING|(OPTION_RIDING_DRAGON)|OPTION_RIDING_WUG|OPTION_MADO) )
