6404d9b9c5c57724b7af5742587ac9c2b4e6c25d
Jobbie <Jobbie@cad27aaa-dce3-4a30-a00a-e4fd67c11881>
2010-03-25 19:18:29 +0000

    * Important merged up of eAthena SVN update from rev14268-71.
    git-svn-id: https://svn.code.sf.net/p/v1-3ceam/code/trunk@287 cad27aaa-dce3-4a30-a00a-e4fd67c11881

--- rewrite/src/map/skill.c
+++ rewrite/src/map/skill.c
@@ -6546,6 +6546,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 				skill_castend_nodamage_id);
 		}
 		break;
+	case NPC_TALK:
 	case ALL_WEWISH:
 		clif_skill_nodamage(src,bl,skillid,skilllv,1);
 		break;
