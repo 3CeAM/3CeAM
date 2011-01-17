9d25ca109028c563b210821a6aeee4bff3dac3d1
Rytech16 <Rytech16@cad27aaa-dce3-4a30-a00a-e4fd67c11881>
2011-01-17 00:43:18 +0000

    *Fixed and updated many Sura skills. *SR_DRAGONCOMBO now works more like a combo and no longer has a range. *SR_SKYNETBLOW damage formula updated. *SR_EARTHSHAKER AoE size around the caster is now affected by skill level. *SR_FALLENEMPIRE now works more like a combo and no longer has a range. -Extra damage from enemy's weight has been disabled, but will be readded later. -Aftercast Delay was removed since combo skills in the middle of the chain shouldnt have them. *SR_TIGERCANNON now works more like a combo and no longer has a range. -Extra damage from the consumed HP has been disabled since it doesent increase skill ratio. -It appears to be used as added on reduceable fixed damage. Will add later. *SR_RAMPAGEBLASTER no longer has a chance to deal 2x or 3x damage when in critical explosion status. -Was disabled since its no longer official. Plus its doing %100 chance of 2x damage for some reason. -Totally pointless to leave this enabled. *SR_LIGHTNINGWALK now has a cooldown of 5 seconds. -Now consumes a % of your HP when used. *SR_KNUCKLEARROW now requires 2 spirit spheres to use. *SR_RAISINGDRAGON Max HP/SP increase corrected to a max of %12 at lv 10. *SR_ASSIMILATEPOWER cast time reduced to 2 seconds. *SR_GENTLETOUCH_CURE now has a 2 cell range. *SR_GENTLETOUCH_CHANGE updated the ammount of HP thats consumed on use. Max is %10 at lv 5. *SR_GENTLETOUCH_REVITALIZE now gives the correct % of Max HP increase. Max is %15 at lv 5. -Removed the added VIT stats since their's no info anywhere about it giving VIT. -It only says about increaseing VIT DEF. *SR_HOWLINGOFLION now deals damage. -Also now requires a skill level number of spheres to use. *SR_RIDEINLIGHTNING cast range is now 9 cells. -Added a 0.5 sec aftercast delay and 1 second cooldown. *Did some code cleanup.
    git-svn-id: svn://svn.code.sf.net/p/v1-3ceam/code/trunk@588 cad27aaa-dce3-4a30-a00a-e4fd67c11881

--- rewrite/src/map/status.c
+++ rewrite/src/map/status.c
@@ -502,7 +502,7 @@ void initChangeTables(void)
 	set_sc( SR_RAISINGDRAGON            , SC_RAISINGDRAGON        , SI_RAISINGDRAGON            , SCB_REGEN|SCB_MAXHP|SCB_MAXSP/*|SCB_ASPD*/ );
 	set_sc( SR_GENTLETOUCH_ENERGYGAIN   , SC_GT_ENERGYGAIN        , SI_GENTLETOUCH_ENERGYGAIN   , SCB_NONE );
 	set_sc( SR_GENTLETOUCH_CHANGE       , SC_GT_CHANGE            , SI_GENTLETOUCH_CHANGE       , SCB_BATK|SCB_ASPD|SCB_DEF|SCB_MDEF );
-	set_sc( SR_GENTLETOUCH_REVITALIZE   , SC_GT_REVITALIZE        , SI_GENTLETOUCH_REVITALIZE   , SCB_VIT|SCB_MAXHP|SCB_DEF2|SCB_REGEN|SCB_ASPD|SCB_SPEED );
+	set_sc( SR_GENTLETOUCH_REVITALIZE   , SC_GT_REVITALIZE        , SI_GENTLETOUCH_REVITALIZE   , SCB_MAXHP|SCB_DEF2|SCB_REGEN|SCB_ASPD|SCB_SPEED );
 
 	set_sc( WA_SWING_DANCE                , SC_SWINGDANCE              , SI_SWINGDANCE                , SCB_SPEED|SCB_ASPD );
 	set_sc( WA_SYMPHONY_OF_LOVER          , SC_SYMPHONYOFLOVER         , SI_SYMPHONYOFLOVERS          , SCB_MDEF );
@@ -3893,8 +3893,6 @@ static unsigned short status_calc_vit(struct block_list *bl, struct status_chang
 		vit += sc->data[SC_MINOR_BBQ]->val1;
 	if(sc->data[SC_INSPIRATION])
 		vit += sc->data[SC_INSPIRATION]->val3;
-	if(sc->data[SC_GT_REVITALIZE])
-		vit += sc->data[SC_GT_REVITALIZE]->val2;
 	if(sc->data[SC_STOMACHACHE])
 		vit -= sc->data[SC_STOMACHACHE]->val1;
 
@@ -4865,11 +4863,11 @@ static unsigned int status_calc_maxhp(struct block_list *bl, struct status_chang
 	if(sc->data[SC_INSPIRATION]) //Custom value.
 		maxhp += maxhp * 3 * sc->data[SC_INSPIRATION]->val1 / 100;
 	if(sc->data[SC_RAISINGDRAGON])
-		maxhp += maxhp / 100 * (2 + sc->data[SC_RAISINGDRAGON]->val1);
+		maxhp += maxhp * (2 + sc->data[SC_RAISINGDRAGON]->val1) / 100;
 	if(sc->data[SC_GT_CHANGE])
 		maxhp -= maxhp * (2 * sc->data[SC_GT_CHANGE]->val1) / 100;
 	if(sc->data[SC_GT_REVITALIZE])
-		maxhp += maxhp * (6 * sc->data[SC_GT_REVITALIZE]->val1) / 100;
+		maxhp += maxhp * (3 * sc->data[SC_GT_REVITALIZE]->val1) / 100;
 	if(sc->data[SC_SOLID_SKIN_OPTION])
 		maxhp += 2000;// Fix amount.
 	if(sc->data[SC_POWER_OF_GAIA])
@@ -4892,7 +4890,7 @@ static unsigned int status_calc_maxsp(struct block_list *bl, struct status_chang
 	if(sc->data[SC_MERC_SPUP])
 		maxsp += maxsp * sc->data[SC_MERC_SPUP]->val2/100;
 	if(sc->data[SC_RAISINGDRAGON])
-		maxsp += maxsp / 100 * (2 + sc->data[SC_RAISINGDRAGON]->val1);
+		maxsp += maxsp * (2 + sc->data[SC_RAISINGDRAGON]->val1) / 100;
 
 	return cap_value(maxsp,1,UINT_MAX);
 }
