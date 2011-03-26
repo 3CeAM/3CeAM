fc5758185d3da7ebc8aaa4eb55a0a6bbef9ba9ca
xazax.hun <xazax.hun@cad27aaa-dce3-4a30-a00a-e4fd67c11881>
2011-03-26 11:33:56 +0000

    * Added some files that I forgot to add in the last revision. * Fixed a crash bug with SR_RAMPAGEBLASTER. * Fixed a typo in GN_CARTCANNON. * Fixed an issue with GN_CHANGEMATERIAL.
    git-svn-id: svn://svn.code.sf.net/p/v1-3ceam/code/trunk@603 cad27aaa-dce3-4a30-a00a-e4fd67c11881

--- rewrite/src/map/battle.c
+++ rewrite/src/map/battle.c
@@ -2267,9 +2267,9 @@ static struct Damage battle_calc_weapon_attack(struct block_list *src, struct bl
 					break;
 				case SR_RAMPAGEBLASTER:
 					if( sc && sc->data[SC_EXPLOSIONSPIRITS] )
-						skillratio += 40 * skill_lv * sd->spiritball_old - 100;
+						skillratio += 40 * skill_lv * (sd?sd->spiritball_old:5) - 100;
 					else
-						skillratio += 20 * skill_lv * sd->spiritball_old - 100;
+						skillratio += 20 * skill_lv * (sd?sd->spiritball_old:5) - 100;
 					break;
 				case SR_KNUCKLEARROW:
 					if( wflag&4 )
