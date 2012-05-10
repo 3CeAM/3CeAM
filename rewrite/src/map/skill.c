bd184e4d228596da1236e93f505539a57a8dfe92
Rytech16 <Rytech16@cad27aaa-dce3-4a30-a00a-e4fd67c11881>
2012-05-10 02:51:45 +0000

    =General - r670 *Royal Guard 2011 update has been applied. -Most of the skills have been updated, but a few skills -couldn't be fully updated due to the complex coding, or -other reasons. More updates will come in the future.
    =Skills
    *LG_BANISHINGPOINT
    -Now has a hit rate that increases with skill level.
    
    *LG_SHIELDPRESS
    -Stun duration updated.
    -Damage formula updated.
    -Stun chance formula updated.
    
    *LG_REFLECTDAMAGE
    -SP requirements updated.
    
    *LG_PINPOINTATTACK
    -SP requirements updated.
    -Now requires a spear to use.
    -Damage formula updated.
    -Status / Equip Break chance formula updated.
    
    *LG_FORCEOFVANGUARD
    -Rage counters now last for 5 minutes.
    -No longer increases caster's DEF since tests on official server
    -shows it doesent increase DEF anymore.
    
    *LG_RAGEBURST
    -Damage formula updated.
    
    *LG_EXEEDBREAK
    -SP requirements updated.
    -Attack boost formula updated.
    
    *LG_OVERBRAND
    -Added missing skill type to the skill database.
    -Corrected knockback to work on the LG_OVERBRAND_BRANDISH ID only as
    -confirmed on official to only work on this ID.
    -SP requirements updated.
    -Damage formula updated.
    
    *LG_PRESTIGE
    -Fixed a issue where Prestige was stackable with Banding.
    -SP consume timer is now every 10 seconds.
    -Updated magic attack dodging formula.
    -Corrected a issue where it was giving soft DEF instead of hard DEF.
    -Updated the DEF increase formula.
    -SP drain per second now affected by skill level.
    
    *LG_BANDING
    -Added missing splash type hex and removed skill type in skill database.
    -Overawe status chance formula updated.
    -Now correctly gives DEF when banding with another Royal Guard.
    -Banding status will now end the Prestige status as their not supposed to stack.
    
    *LG_MOONSLASHER
    -Now requires a spear to use.
    
    *LG_RAYOFGENESIS
    -SP requirements updated.
    -Damage formula updated.
    
    *LG_EARTHDRIVE
    -Damage formula updated.
    -Shield break chance updated.
    
    *LG_HESPERUSLIT
    -Now requires a spear to use.
    -Damage formula updated.
    
    *LG_INSPIRATION
    -SP requirements updated.
    -Now requires 1% of the caster's exp to cast.
    -Updated HIT increase formula.
    -Note: HIT increase formula will temporarily give the max increase a
    -players job level at level 50 can give until a fix for the complex
    -system can be made. More info in the 2011 update questions document.
    -Updated MaxHP increase formula.
    -Updated stat's increase formula.
    -HP and SP drain per second is updated.
    -Now make's the caster immune to the following status's
    -Poison / Blind / Stun / Silence / Chaos / Petrification
    -Sleep / Bleeding / Curse / Burning / Freezing / Frozen
    -Crystalization / Fear / Toxin / Paralyze / Venombleed
    -Magic Mushroom / Disheart / Pyrexia / Oblivion Curse
    -Leech End / Deep Sleep / Saturday Night Fever / Paint Body
    -Masquerade: Enervation, Gloomy, Ignorance, Laziness, Unlucky, Weakness.
    
    
    =Database
    *Fixed a issue where the Chrome 2-Handed Sword could not be equipped by Knights or higher.
    
    *Disabled the MATK parts on some items in the item_db2.txt that caused error's to appear on
    -the map server console since added fixed amounts of MATK is not supported yet.
    
    
    =Source
    *Changed a few HP checks in battle.c.
    
    *Did many code cleanup's and optimization.
    
    
    git-svn-id: https://svn.code.sf.net/p/v1-3ceam/code/trunk@671 cad27aaa-dce3-4a30-a00a-e4fd67c11881

--- rewrite/src/map/skill.c
+++ rewrite/src/map/skill.c
@@ -1126,10 +1126,10 @@ int skill_additional_effect (struct block_list* src, struct block_list *bl, int 
 			skill_castend_damage_id(src, bl, NC_AXEBOOMERANG, pc_checkskill(sd, NC_AXEBOOMERANG), tick, 1);
 		break;
 	case LG_SHIELDPRESS:
-		sc_start(bl, SC_STUN, 30 + 8 * skilllv, skilllv, skill_get_time(skillid,skilllv));
+		sc_start(bl, SC_STUN, 30 + 8 * skilllv + sstatus->dex / 10 + sd->status.job_level / 4, skilllv, skill_get_time(skillid,skilllv));
 		break;	
 	case LG_PINPOINTATTACK:
-		rate = 12 + (10 * skilllv + (sstatus->agi / 100) ) * 140 / 100;
+		rate = 30 + 5 * (sd ? pc_checkskill(sd,LG_PINPOINTATTACK) : 1) + (sstatus->agi + status_get_lv(bl)) / 10;
 		switch( skilllv )
 		{
 			case 1: sc_start(bl,SC_BLEEDING,rate,skilllv,skill_get_time(skillid,skilllv)); break;
@@ -1149,7 +1149,7 @@ int skill_additional_effect (struct block_list* src, struct block_list *bl, int 
 			sc_start(bl, SC_BLIND,50, skilllv, skill_get_time(skillid,skilllv));
 		break;
 	case LG_EARTHDRIVE:
-		skill_break_equip(src, EQP_SHIELD, 500, BCT_SELF);
+		skill_break_equip(src, EQP_SHIELD, 100 * skilllv, BCT_SELF);
 		sc_start(bl, SC_EARTHDRIVE, 100, skilllv, skill_get_time(skillid, skilllv));
 		break;
 	case SR_DRAGONCOMBO:
@@ -2383,6 +2383,7 @@ int skill_attack(int attack_type, struct block_list* src, struct block_list *dsr
 			case PR_SANCTUARY:
 			case SC_TRIANGLESHOT:
 			case LG_OVERBRAND:
+			//case LG_OVERBRAND_BRANDISH://To be enabled in the future when the skill workings are updated.
 			case SR_KNUCKLEARROW:
 			case GN_WALLOFTHORN:
 			case EL_FIRE_MANTLE:
@@ -7900,6 +7901,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 		break;
 		
 	case LG_TRAMPLE:
+		//if (rand()%100 < 25+25*skilllv)// Enable when confirmed on test server if animation happens reguardless.
 		clif_skill_damage(src,bl,tick, status_get_amotion(src), 0, -30000, 1, skillid, skilllv, 6);
 		map_foreachinrange(skill_destroy_trap,bl,skill_get_splash(skillid,skilllv),BL_SKILL,tick);
 		break;
@@ -8048,8 +8050,8 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 		{
 			if( !map[src->m].flag.noexppenalty )
 			{
-				sd->status.base_exp -= min(sd->status.base_exp, pc_nextbaseexp(sd) * 1 / 1000); //.1% penalty.
-				sd->status.job_exp -= min(sd->status.job_exp, pc_nextjobexp(sd) * 1 / 1000);
+				sd->status.base_exp -= min(sd->status.base_exp, pc_nextbaseexp(sd) * 1 / 100); //1% penalty.
+				sd->status.job_exp -= min(sd->status.job_exp, pc_nextjobexp(sd) * 1 / 100);
 				clif_updatestatus(sd,SP_BASEEXP);
 				clif_updatestatus(sd,SP_JOBEXP);
 			}
@@ -11296,7 +11298,10 @@ int skill_unit_onplace_timer (struct skill_unit *src, struct block_list *bl, uns
 
 		case UNT_BANDING:
 			if( battle_check_target(ss,bl,BCT_ENEMY) > 0 && !(status_get_mode(bl)&MD_BOSS) && !(tsc && tsc->data[SC_BANDING_DEFENCE]) )
-				sc_start(bl,SC_BANDING_DEFENCE,100,90,skill_get_time2(sg->skill_id,sg->skill_lv));
+				if( battle_config.renewal_baselvl_skill_ratio == 1 && status_get_lv(bl) >= 100 )
+				sc_start(bl,SC_BANDING_DEFENCE,sd->status.base_level / 5 + 5 * sg->skill_lv - tstatus->agi / 10,90,skill_get_time2(sg->skill_id,sg->skill_lv));
+				else
+				sc_start(bl,SC_BANDING_DEFENCE,30 + 5 * sg->skill_lv - tstatus->agi / 10,90,skill_get_time2(sg->skill_id,sg->skill_lv));
 			break;
 
 		case UNT_FIRE_MANTLE:
@@ -14064,7 +14069,7 @@ static int skill_trap_splash (struct block_list *bl, va_list ap)
 int skill_enchant_elemental_end (struct block_list *bl, int type)
 {
 	struct status_change *sc;
-	const enum sc_type scs[] = { SC_ENCPOISON, SC_ASPERSIO, SC_FIREWEAPON, SC_WATERWEAPON, SC_WINDWEAPON, SC_EARTHWEAPON, SC_SHADOWWEAPON, SC_GHOSTWEAPON, SC_ENCHANTARMS, SC__INVISIBILITY, SC_EXPIATIO, SC_EXEEDBREAK };//Why's invisibility and Exeed Break here? [Rytech]
+	const enum sc_type scs[] = { SC_ENCPOISON, SC_ASPERSIO, SC_FIREWEAPON, SC_WATERWEAPON, SC_WINDWEAPON, SC_EARTHWEAPON, SC_SHADOWWEAPON, SC_GHOSTWEAPON, SC_ENCHANTARMS, SC__INVISIBILITY };
 	int i;
 	nullpo_ret(bl);
 	nullpo_ret(sc= status_get_sc(bl));
